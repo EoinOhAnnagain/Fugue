7be74c93a76d46e4fba85f1620a46c3a8522001c
Eoin <eoin.ohannagain@ucdconnect.ie>
2023-06-11 21:45:44 +0100

    Example config initial commit

---
+++ backend/configExample.py
@@ -0,0 +1,12 @@
+#Example version of config.py
+
+host = "" #Insert host name here.
+user = "" #Insert user here.
+password = "" #Insert password here.
+database = "" #Insert database to be accessed here.
+
+secret_key = "" #Insert secret key for JWT.
+
+salts = [""] #Insert list of salts here.
+
+origins = [""] #Insert list of approved origins here.
