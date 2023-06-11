0570e518d6bb430335587e0edb4f7d3f71c05f8b
Eoin <eoin.ohannagain@ucdconnect.ie>
2023-06-11 21:38:56 +0100

    Changed return message

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,19 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, Flask!'
+    return 'Hello :)'
 
 @app.route('/about')
 def about():
     return 'about found'
+
+@app.route('/newQueueItem', methods=['GET'])
+def newQueueEntry():
+    #return "First Name = ", request.json['firstName'] , " Last Name = ", request.json['lastName']
+    return jsonify({"test1": "test", "test2": "test2"})
 
 
 if __name__ == '__main__':
