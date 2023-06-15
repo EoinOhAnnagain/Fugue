3d1417cecb23080aacd21f1d45c0067d1289e55b
Eoin <eoin.ohannagain@ucdconnect.ie>
2023-06-15 21:32:30 +0100

    Add method to get names of queues

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,21 @@
+from textwrap import wrap
 from flask import Flask, request, jsonify
 import config, uuid, mysql.connector
 from mysql.connector import errorcode
+from functools import wraps
 
 app = Flask(__name__)
 
 
+def getStarted(f):
+    @wraps(f)
+    def getSetUp(*args, **kwargs):
+
+        db = create_db_connection()
+        cursor = db.cursor()
+
+        return f(db, cursor, *args, **kwargs)
+    return getSetUp
 
 """ Method to create connection with the database. Returns the database connection if successful. Returns False if there is an error and prints the error """
 def create_db_connection():
@@ -19,12 +30,56 @@ def create_db_connection():
         else:
             return False
 
+def getQueueNames(cursor):
+    cursor.execute("SELECT * FROM `Queues`")
+    queues = cursor.fetchall()
+    returnable = []
+    for q in queues:
+        print(q)
+        print(q[0])
+        returnable.append(q[0])
+    print(queues)
+    print(returnable)
+    return returnable
+    
+
 
 
 
 @app.route('/')
 def index():
     return 'Hello :)' + str(uuid.uuid4().hex) + ':P'
+
+@app.route('/getQueueNames')
+@getStarted
+def getQueues(db, cursor):
+
+    print("test")
+
+    if db != False:
+
+        print('db found')
+        # Get Employee data as a list of dictionaries and turn it into a JSON object
+        json_dump = jsonify(getQueueNames(cursor))
+
+        print(json_dump)
+
+        # Close db connection
+        cursor.close()
+        db.close()
+
+        print("oh yeah")
+
+        # Return JSON and status code
+        return json_dump, 200
+    else:
+        cursor.close()
+        db.close()
+        return jsonify({'Error': "Database Connection Error"}), 502
+
+    
+
+
 
 @app.route('/checkFullQueue', methods=['GET'])
 def checkFullQueue():
